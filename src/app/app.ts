c41d7f3f12c91d449c9fb1dc56529ac478feaffd
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-16 12:33:56 +0300

    add slider

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "../copmonents/nav-bar/nav-bar";
+import { Slider } from "../copmonents/slider/slider";
 
 @Component({
   selector: 'app-root',
-  imports: [ NavBar],
+  imports: [NavBar, Slider],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
