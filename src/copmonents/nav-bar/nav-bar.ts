829e5c503fc14ebe1876773709af46891f6f5299
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-19 13:11:00 +0300

    add dark mode for nav bar

--- src/copmonents/nav-bar/nav-bar.ts
+++ src/copmonents/nav-bar/nav-bar.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
-
+  isLightMode =true;
+  toggleTheme(){
+this.isLightMode = !this.isLightMode;
+  document.body.classList.toggle('dark-mode', !this.isLightMode);
+  }
 }
