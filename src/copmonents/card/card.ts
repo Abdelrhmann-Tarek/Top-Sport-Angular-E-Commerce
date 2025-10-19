82fd291d906b7b4ae73845947599882a4b9c1bf5
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-19 13:48:10 +0300

    initial filtreation commit

--- src/copmonents/card/card.ts
+++ src/copmonents/card/card.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.html',
   styleUrl: './card.css'
 })
 export class Card {
-
+  cards = [
+    { title: 'Card 1',department:'Shoeses', description: 'This is the first card', image: 'assets/img1.jpg' },
+    { title: 'Card 2',department:'Shoeses', description: 'This is the second card', image: 'assets/img2.jpg' },
+    { title: 'Card 3',department:'shirts', description: 'This is the third card', image: 'assets/img3.jpg' },
+    { title: 'Card 4',department:'shirts', description: 'This is the fourth card', image: 'assets/img4.jpg' },
+    { title: 'Card 5',department:'bags', description: 'This is the fifth card', image: 'assets/img5.jpg' },
+    { title: 'Card 6',department:'bags', description: 'This is the sixth card', image: 'assets/img6.jpg' }
+  ];
 }
+
