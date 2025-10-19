a1527904cb8d917280b5cc8ee729f53ecd4dfbfa
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-19 14:22:25 +0300

    sss

--- src/copmonents/card/card.ts
+++ src/copmonents/card/card.ts
@@ -9,8 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './card.css'
 })
 export class Card {
-    selectedDepartment: string = 'All';
-
+  
   cards = [
     { title: 'Card 1',department:'Shoeses', description: 'This is the first card', image: './product1.jpg' },
     { title: 'Card 2',department:'Shoeses', description: 'This is the second card', image: './product2.jpg' },
@@ -19,23 +18,19 @@ export class Card {
     { title: 'Card 5',department:'bags', description: 'This is the fifth card', image: './product5.jpg' },
     { title: 'Card 6',department:'bags', description: 'This is the sixth card', image: './product6.jpg' }
   ];
+  selectedDepartment: string = 'All';
+
+  
    get departments() {
     return ['All', ...new Set(this.cards.map(card => card.department))];
   }
 
-  // Filter logic
   get filteredCards() {
     return this.selectedDepartment === 'All'
       ? this.cards
       : this.cards.filter(card => card.department === this.selectedDepartment);
   }
-  // selectedCategory:string = "All";
-  //   filteredProducts = this.cards;
-  
-  //   filterByCategory() {
-  //     this.filteredProducts = this.selectedCategory === 'All'
-  //       ? this.cards
-  //       : this.cards.filter(p => p.department === this.selectedCategory);
+
 }
 
   
