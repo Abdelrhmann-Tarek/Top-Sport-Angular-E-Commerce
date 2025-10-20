dcc5399ad6df0687deee0c8801567bf86c6e4ae2
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-20 14:07:21 +0300

    add routes

--- src/directive/hover-card.ts
+++ src/directive/hover-card.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,  HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHoverCard]'
 })
 export class HoverCard {
 
-  constructor() { }
-
+ constructor(private el: ElementRef) {
 }
+@HostListener('mouseenter') onMouseEnter(){
+  this.el.nativeElement.style.transform='scale(1.1)';
+  this.el.nativeElement.style.transition='transform 0.3s ease-in-out';
+}
+
+@HostListener('mouseleave') onMouseLeave(){
+  this.el.nativeElement.style.transform ='scale(1)';
+  this.el.nativeElement.style.transition ='transform 0.3s ease-in-out';
+}
+}
+
