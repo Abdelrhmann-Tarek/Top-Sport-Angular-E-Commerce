3ffae3c46382d51596c90db1ef133ff6de5fa9b5
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-20 13:13:08 +0300

    add routes

--- src/copmonents/best-seller/best-seller.ts
+++ src/copmonents/best-seller/best-seller.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Grid } from "../grid/grid";
 
 @Component({
   selector: 'app-best-seller',
-  imports: [],
+  imports: [Grid],
   templateUrl: './best-seller.html',
   styleUrl: './best-seller.css'
 })
