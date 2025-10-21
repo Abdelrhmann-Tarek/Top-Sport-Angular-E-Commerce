e6d6683d02e690c6148bb79a60ad349eba07512f
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-10-21 10:40:29 +0300

    changes in slider

--- src/copmonents/home/home.ts
+++ src/copmonents/home/home.ts
@@ -4,7 +4,7 @@ import { Grid } from "../grid/grid";
 
 @Component({
   selector: 'app-home',
-  imports: [Slider],
+  imports: [Slider, Grid],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
